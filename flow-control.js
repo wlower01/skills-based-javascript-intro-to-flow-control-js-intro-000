4a94fd050ed22c57469772538e153106fe751e33
rational-block-1a0e <william.lower2477@gmail.com>
2021-06-02 22:23:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -20,5 +20,20 @@ return (age >=12 && age <=20) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
+ var age = "19"
+ switch(mood) {
+   case "13" :
+   console.log("You are a teenager");
+   break;
+   case "14" :
+   console.log("You are a teenager");
+   break;
+   case "15" :
+   console.log("You are a teenager");
+   break;
+   case "16" :
+   console.log("You are a teenager");
+   break;
 
+ }
 }

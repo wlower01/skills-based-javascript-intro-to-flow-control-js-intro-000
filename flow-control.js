5eea8959e1bdaaa70c447f6556579b9207ef2751
rational-block-1a0e <william.lower2477@gmail.com>
2021-06-02 21:55:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -9,12 +9,13 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (age <=13) {return "You are a kid"}
+  if (age >=13 && age ,19) {return "You are a teenager!"}
   else if (age >=19) {return "You are a grownup"}
-    else {"You are a teenager!"}
+    else {"You are a kid"}
   }
 
 function ternaryTeenager(age) {
+return (age >=13 && age <=19) ? "You are a teenager!" : "You are not a teenager"
 
 }
 
